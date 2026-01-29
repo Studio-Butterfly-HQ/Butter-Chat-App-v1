8f36f2920847fcd84c48bd7dcef7595564a5dfbf
shomik-das <dashomik@gmail.com>
2026-01-29 17:36:56 +0600

    add get and post api for the department page

--- Frontend/src/constants/api.ts
+++ Frontend/src/constants/api.ts
@@ -2,21 +2,26 @@ export const BACKEND_BASE_URL_LOCAL = "http://localhost:5599"
 export const BACKEND_BASE_URL = "https://api.studiobutterfly.io"
 
 export const AUTH_API = {
-    LOGIN: `${BACKEND_BASE_URL}/auth/login`,
-    SIGNUP: `${BACKEND_BASE_URL}/auth/register`,
-    RESET_PASSWORD: `${BACKEND_BASE_URL}/auth/reset-password`,
+  LOGIN: `${BACKEND_BASE_URL}/auth/login`,
+  SIGNUP: `${BACKEND_BASE_URL}/auth/register`,
+  RESET_PASSWORD: `${BACKEND_BASE_URL}/auth/reset-password`,
 }
 export const COMPANY_API = {
-    UPDATE_PROFILE: `${BACKEND_BASE_URL}/company/update`,
-    GET_PROFILE: `${BACKEND_BASE_URL}/company/profile`,
+  UPDATE_PROFILE: `${BACKEND_BASE_URL}/company/update`,
+  GET_PROFILE: `${BACKEND_BASE_URL}/company/profile`,
 }
 
 export const CONNECTIONS_API = {
-    FACEBOOK_LOGIN: `${BACKEND_BASE_URL}/auth/meta/login`,
+  FACEBOOK_LOGIN: `${BACKEND_BASE_URL}/auth/meta/login`,
 }
 export const SHIFT_API = {
-    CREATE_SHIFT: `${BACKEND_BASE_URL}/shift`,
-    GET_SHIFTS: `${BACKEND_BASE_URL}/shift`
-}
+  CREATE_SHIFT: `${BACKEND_BASE_URL}/shift`,
+  GET_SHIFTS: `${BACKEND_BASE_URL}/shift`,
+};
+
+export const DEPARTMENT_API = {
+  CREATE_DEPARTMENT: `${BACKEND_BASE_URL}/department`,
+  GET_DEPARTMENTS: `${BACKEND_BASE_URL}/department`,
+};
 
 export const COUNTRY_API_URL = "https://restcountries.com/v3.1/all?fields=name,cca2,languages,timezones"
---
+++ Frontend/src/provider/department/department.api.ts
@@ -0,0 +1,40 @@
+import type {
+  CreateDepartmentPayload,
+  ApiResponse,
+  Department,
+} from "./department.types";
+import { DEPARTMENT_API } from "@/constants/api";
+
+export const getDepartmentsApi = async (token: string): Promise<ApiResponse<Department[]>> => {
+  console.log("api request");
+  const res = await fetch(`${DEPARTMENT_API.GET_DEPARTMENTS}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await res.json();
+  if (!res.ok) {
+    throw data;
+  }
+
+  return data;
+};
+
+export const createDepartmentApi = async ( payload: CreateDepartmentPayload, token: string): Promise<ApiResponse<Department>> => {
+  const res = await fetch(`${DEPARTMENT_API.CREATE_DEPARTMENT}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(payload),
+  });
+  const data = await res.json();
+  if (!res.ok) {
+    throw data;
+  }
+
+  return data;
+};
