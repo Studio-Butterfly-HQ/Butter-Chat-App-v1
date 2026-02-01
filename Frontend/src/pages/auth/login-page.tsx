a7b6ca5badeb90d38738ae88961b2c6a9dec39a1
shomik-das <dashomik@gmail.com>
2026-02-01 12:03:05 +0600

    fix login signup and reset page

--- Frontend/src/pages/auth/login-page.tsx
+++ Frontend/src/pages/auth/login-page.tsx
@@ -2,7 +2,6 @@ import { useState} from "react"
 import { Link } from "react-router-dom"
 import { useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
 import { Card } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
 import { ScrollArea } from "@/components/ui/scroll-area"
@@ -43,9 +42,9 @@ export default function LoginPage() {
   const handleSubmit = async (data: LoginFormValues) => {
     try {
       const res = await mutateAsync(data)
-        if (res.success) {
+      if (res.success) {
           navigate("/dashboard")
-        }
+      }
     }
     catch (error) {
       console.error("Error in login page: ", error)
@@ -57,152 +56,162 @@ export default function LoginPage() {
       <div className="w-full lg:w-1/2 h-screen flex flex-col">
         <div className="flex items-center gap-2 p-6 lg:p-8">
           <MessageCircle className="text-primary text-2xl" />
-          <span className="text-primary text-2xl font-medium">
-            ButterChat
-          </span>
+          <span className="text-primary text-2xl font-medium">ButterChat</span>
         </div>
 
-        <div className="flex-1 h-0 min-h-0 pb-3">
+        <div className="flex-1 h-0 min-h-0"> {/* todo pb-3 */}
           <ScrollArea className="h-full">
-          <div className="px-6 lg:px-8 pb-6 lg:pb-8">
-            <div className="md:p-8 pt-8 flex items-center justify-center min-h-full">
-              <div className="w-full max-w-md">
-              <Card className="bg-transparent border-0 shadow-none">
-              <div className="flex flex-col items-center gap-2 mb-6">
-                <h1 className="text-primary text-3xl font-bold text-center">
-                  Login to your account
-                </h1>
-                <p className="text-muted-foreground text-lg text-center">
-                  Enter your credentials to access your account
-                </p>
-              </div>
+            <div className="px-6 lg:px-8 pb-6 lg:pb-8 min-h-[calc(100vh-6rem)] flex flex-col justify-center">
+              <div className="md:p-8 pt-8 flex items-center justify-center">
+                <div className="w-full max-w-md">
+                  <Card className="bg-transparent border-0 shadow-none">
+                    <div className="flex flex-col items-center gap-2 mb-6">
+                      <h1 className="text-primary text-3xl font-bold text-center">
+                        Login to your account
+                      </h1>
+                      <p className="text-muted-foreground text-lg text-center">
+                        Enter your credentials to access your account
+                      </p>
+                    </div>
 
-              <Form {...form}>
-                <form
-                  onSubmit={form.handleSubmit(handleSubmit)}
-                  className="flex flex-col gap-4 mb-6"
-                >
-                  {/* Email */}
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel className="text-primary text-base font-semibold">
-                          Email
-                        </FormLabel>
-                        <EmailInput
-                          value={field.value}
-                          onChange={field.onChange}
-                          onBlur={field.onBlur}
+                    <Form {...form}>
+                      <form
+                        onSubmit={form.handleSubmit(handleSubmit)}
+                        className="flex flex-col gap-4 mb-6"
+                      >
+                        {/* Email */}
+                        <FormField
+                          control={form.control}
+                          name="email"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel className="text-primary text-base font-semibold">
+                                Email
+                              </FormLabel>
+                              <EmailInput
+                                value={field.value}
+                                onChange={field.onChange}
+                                onBlur={field.onBlur}
+                              />
+                              <FormMessage className="text-sm" />
+                            </FormItem>
+                          )}
                         />
-                        <FormMessage className="text-sm" />
-                      </FormItem>
-                    )}
-                  />
 
-                  {/* Password */}
-                  <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem>
-                        <div className="flex items-center justify-between">
-                          <FormLabel className="text-primary text-base font-semibold">
-                            Password
-                          </FormLabel>
-                          <Link
-                            to="/reset-password"
-                            className="text-sm text-primary underline"
-                          >
-                            Forgot password?
-                          </Link>
-                        </div>
+                        {/* Password */}
+                        <FormField
+                          control={form.control}
+                          name="password"
+                          render={({ field }) => (
+                            <FormItem>
+                              <div className="flex items-center justify-between">
+                                <FormLabel className="text-primary text-base font-semibold">
+                                  Password
+                                </FormLabel>
+                                <Link
+                                  to="/reset-password"
+                                  className="text-sm text-primary underline"
+                                >
+                                  Forgot password?
+                                </Link>
+                              </div>
 
-                        <InputGroup>
-                          <InputGroupInput
-                            {...field}
-                            type={showPassword ? "text" : "password"}
-                            placeholder="••••••••••••"
-                          />
-                          <InputGroupAddon
-                            align="inline-end"
-                            className="cursor-pointer bg-background h-full rounded-r-md"
-                            onClick={() =>
-                              setShowPassword(!showPassword)
-                            }
-                          >
-                            {showPassword ? <EyeOff /> : <Eye />}
-                          </InputGroupAddon>
-                        </InputGroup>
+                              <InputGroup>
+                                <InputGroupInput
+                                  {...field}
+                                  type={showPassword ? "text" : "password"}
+                                  placeholder="••••••••••••"
+                                />
+                                <InputGroupAddon
+                                  align="inline-end"
+                                  className="cursor-pointer bg-background h-full rounded-r-md"
+                                  onClick={() => setShowPassword(!showPassword)}
+                                >
+                                  {showPassword ? <EyeOff /> : <Eye />}
+                                </InputGroupAddon>
+                              </InputGroup>
 
-                        {/* <p className="text-muted-foreground text-sm">
+                              {/* <p className="text-muted-foreground text-sm">
                           Must be at least 8 characters
                         </p> */}
-                        <FormMessage className="text-sm" />
-                      </FormItem>
-                    )}
-                  />
+                              <FormMessage className="text-sm" />
+                            </FormItem>
+                          )}
+                        />
 
-                  <Button className="rounded-lg font-medium" disabled={isPending}>
-                    {isPending ? <> <Spinner /> Please wait... </>: <> Login </>}
-                  </Button>
-                </form>
-              </Form>
+                        <Button
+                          className="rounded-lg font-medium"
+                          disabled={isPending}
+                        >
+                          {isPending ? (
+                            <>
+                              {" "}
+                              <Spinner /> Please wait...{" "}
+                            </>
+                          ) : (
+                            <> Login </>
+                          )}
+                        </Button>
+                      </form>
+                    </Form>
 
-              <div className="flex items-center gap-3 mb-4">
-                <Separator className="flex-1" />
-                <span className="text-md text-muted-foreground">
-                  Or continue with
-                </span>
-                <Separator className="flex-1" />
-              </div>
+                    <div className="flex items-center gap-3 mb-4">
+                      <Separator className="flex-1" />
+                      <span className="text-md text-muted-foreground">
+                        Or continue with
+                      </span>
+                      <Separator className="flex-1" />
+                    </div>
 
-              <div className="flex gap-4 mb-4">
-                <Button variant="outline" className="flex-1 rounded-xl">
-                  <img
-                    src="/icons/facebook.svg"
-                    alt="Facebook"
-                    className="h-5 w-5"
-                  />
-                </Button>
-                <Button variant="outline" className="flex-1 rounded-xl">
-                  <img
-                    src="/icons/google.svg"
-                    alt="Google"
-                    className="h-5 w-5"
-                  />
-                </Button>
-                <Button variant="outline" className="flex-1 rounded-xl">
-                  <img
-                    src="/icons/linkedin.svg"
-                    alt="LinkedIn"
-                    className="h-5 w-5"
-                  />
-                </Button>
-              </div>
+                    <div className="flex gap-4 mb-4">
+                      <Button variant="outline" className="flex-1 rounded-xl">
+                        <img
+                          src="/icons/facebook.svg"
+                          alt="Facebook"
+                          className="h-5 w-5"
+                        />
+                      </Button>
+                      <Button variant="outline" className="flex-1 rounded-xl">
+                        <img
+                          src="/icons/google.svg"
+                          alt="Google"
+                          className="h-5 w-5"
+                        />
+                      </Button>
+                      <Button variant="outline" className="flex-1 rounded-xl">
+                        <img
+                          src="/icons/linkedin.svg"
+                          alt="LinkedIn"
+                          className="h-5 w-5"
+                        />
+                      </Button>
+                    </div>
 
-              <p className="text-center text-muted-foreground mb-4">
-                Don’t have an account?{" "}
-                <Link to="/signup" className="underline text-muted-foreground">
-                  Sign up
-                </Link>
-              </p>
+                    <p className="text-center text-muted-foreground mb-4">
+                      Don’t have an account?{" "}
+                      <Link
+                        to="/signup"
+                        className="underline text-muted-foreground"
+                      >
+                        Sign up
+                      </Link>
+                    </p>
 
-              <Separator className="mb-4" />
+                    <Separator className="mb-4" />
 
-              <p className="text-center text-muted-foreground text-sm">
-                By clicking continue, you agree to our{" "}
-                <span className="underline cursor-pointer">Terms</span>{" "}
-                and{" "}
-                <span className="underline cursor-pointer">
-                  Privacy Policy
-                </span>.
-              </p>
-              </Card>
+                    <p className="text-center text-muted-foreground text-sm">
+                      By clicking continue, you agree to our{" "}
+                      <span className="underline cursor-pointer">Terms</span>{" "}
+                      and{" "}
+                      <span className="underline cursor-pointer">
+                        Privacy Policy
+                      </span>
+                      .
+                    </p>
+                  </Card>
+                </div>
               </div>
             </div>
-          </div>
           </ScrollArea>
         </div>
       </div>
@@ -215,5 +224,5 @@ export default function LoginPage() {
         />
       </div>
     </div>
-  )
+  );
 }
